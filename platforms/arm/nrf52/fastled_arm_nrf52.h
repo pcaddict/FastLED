df3ceceabc92c23c8256e57e899ae04d00b6a326
Patrick Grindstaff <patrick@tnsgo.com>
2018-11-29 20:54:46 -0500

    Add platform files for NRF52.

---
+++ platforms/arm/nrf52/fastled_arm_nrf52.h
@@ -0,0 +1,9 @@
+#ifndef __INC_FASTLED_ARM_NRF52_H
+#define __INC_FASTLED_ARM_NRF52_H
+
+// Include the k20 headers
+#include "fastpin_arm_nrf52.h"
+#include "fastspi_arm_nrf52.h"
+//#include "clockless_arm_nrf52.h"
+
+#endif
---
+++ platforms/arm/nrf52/led_sysdefs_arm_nrf52.h
@@ -0,0 +1,46 @@
+#ifndef __LED_SYSDEFS_ARM_NRF52
+#define __LED_SYSDEFS_ARM_NRF52
+
+#ifndef NRF52
+#define NRF52
+#endif
+
+#define LED_TIMER NRF_TIMER3	//NRF_TIMER1 used by Arduino BSP.
+#define FASTLED_NO_PINMAP
+//#define FASTLED_HAS_CLOCKLESS
+
+//#define FASTLED_SPI_BYTE_ONLY
+
+#define FASTLED_ARM
+//#define FASTLED_ARM_M0
+
+#ifndef F_CPU
+#define F_CPU 64000000
+#endif
+
+#include <stdint.h>
+#include <nrf52.h>
+//#include <core_cm0.h>
+
+typedef volatile uint32_t RoReg;
+typedef volatile uint32_t RwReg;
+typedef uint32_t prog_uint32_t;
+//typedef uint8_t boolean;
+
+#define PROGMEM
+#define NO_PROGMEM
+#define NEED_CXX_BITS
+
+// Default to NOT using PROGMEM here
+#ifndef FASTLED_USE_PROGMEM
+#define FASTLED_USE_PROGMEM 0
+#endif
+
+#ifndef FASTLED_ALLOW_INTERRUPTS
+#define FASTLED_ALLOW_INTERRUPTS 1
+#endif
+
+#define cli()  __disable_irq();
+#define sei() __enable_irq();
+
+#endif
